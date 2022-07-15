93f97563b7cb1b6b8003844efb9e588c8818a86d
Samuelr499 <samuelribeiro2@hotmail.com>
2022-07-15 11:25:59 -0300

    feat: adiciona searchBar, adiciona rotas no app

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Header from '../../components/Header';
 import profileIcon from '../../images/profileIcon.svg';
 
-export default function Profile() {
+export default function Profile(props) {
+  const { history } = props;
   return (
-    <Header showSearchIcon iconProfile={ profileIcon } currentPage="Profile" />
+    <Header
+      showSearchIcon={ false }
+      iconProfile={ profileIcon }
+      currentPage="Profile"
+      history={ history }
+      isInProfile
+    />
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape.isRequired,
+};
