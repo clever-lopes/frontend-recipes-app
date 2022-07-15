1e6d1c47dbb48ae136cb81e0970caba7055dafef
Samuelr499 <samuelribeiro2@hotmail.com>
2022-07-15 14:10:43 -0300

    feat: adiciona Footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
+import Footer from '../../components/Footer';
 
 export default function Foods(props) {
   const { history } = props;
@@ -18,6 +19,7 @@ export default function Foods(props) {
       <main>
         <h1>Hello World</h1>
       </main>
+      <Footer />
     </body>
   );
 }
