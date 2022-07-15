93a44f5f5b3d1211b9718a7f33b1f2939c065c5f
Victorpagotto <victor.pagotto12@gmail.com>
2022-07-15 20:50:55 -0300

    Criada a lista de receitas.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,14 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/login/Login';
 import NotFound from '../pages/notFound/index';
 import Profile from '../pages/profile/Profile';
-import Home from '../pages/home/home';
+import Recipes from '../pages/home/Recipes';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/foods" component={ Home } />
-      <Route path="/drinks" component={ Home } />
+      <Route path="/foods" component={ Recipes } />
+      <Route path="/drinks" component={ Recipes } />
       <Route path="/profile" component={ Profile } />
       <Route path="/*" component={ NotFound } />
     </Switch>
