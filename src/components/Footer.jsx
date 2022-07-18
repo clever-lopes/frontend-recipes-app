d7e5c64d0cd0f8b6cd5fd8db26acd7c303076a71
Victorpagotto <victor.pagotto12@gmail.com>
2022-07-18 19:41:55 -0300

    Feito a tela de receitas.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,20 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import './CSS/Footer.css';
 
 export default function Footer() {
   const history = useHistory();
   return (
     <footer
       data-testid="footer"
+      className="footer-container"
     >
       <button
         type="button"
         data-testid="drinks-bottom-btn"
         onClick={ () => history.push('/drinks') }
+        src={ drinkIcon }
       >
         <img src={ drinkIcon } alt="drinkIcon" />
       </button>
@@ -21,6 +24,7 @@ export default function Footer() {
         type="button"
         data-testid="food-bottom-btn"
         onClick={ () => history.push('/foods') }
+        src={ mealIcon }
       >
         <img src={ mealIcon } alt="mealIcon" />
       </button>
