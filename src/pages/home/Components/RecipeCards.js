55d13bf516e4ea0965653d4b1a8f46a23da2fa37
Victorpagotto <victor.pagotto12@gmail.com>
2022-07-18 18:25:45 -0300

    Arrumado bug de searchRecipe na SearchBar

--- src/pages/home/Components/RecipeCards.js
+++ src/pages/home/Components/RecipeCards.js
@@ -7,8 +7,25 @@ export default function RecipeCards() {
   return (
     <div>
       {
-        productList.map((product, id) => (
-          <p key={ id }>{ product.Meal || product.Drink }</p>
+        productList.map((product, index) => (
+          <div key={ `data-testid="${index}-recipe-card"` }>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ product.MealThumb || product.DrinkThumb }
+              alt={ product.Meal || product.Drink }
+            />
+            <div>
+              <p
+                data-testid={ `${index}-card-name` }
+              >
+                { product.Meal || product.Drink }
+              </p>
+              <div>
+                <p>{ product.Area }</p>
+                <p>{ product.Category }</p>
+              </div>
+            </div>
+          </div>
         ))
       }
     </div>
