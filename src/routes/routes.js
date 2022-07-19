60005e4f3731d1f26c70cb6f2a5fa5401652ba1a
Victorpagotto <victor.pagotto12@gmail.com>
2022-07-19 19:51:04 -0300

    In progress.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,14 @@ import Login from '../pages/login/Login';
 import NotFound from '../pages/notFound/index';
 import Profile from '../pages/profile/Profile';
 import Recipes from '../pages/home/Recipes';
+import Progress from '../pages/progress/Progress';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/foods/:id/in-progress" component={ Progress } />
+      <Route path="/drinks/:id/in-progress" component={ Progress } />
       <Route path="/foods" component={ Recipes } />
       <Route path="/drinks" component={ Recipes } />
       <Route path="/profile" component={ Profile } />
