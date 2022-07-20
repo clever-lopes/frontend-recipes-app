22a8f1086b4be26f7b059358c6af41eb2135de50
Samuelr499 <samuelribeiro2@hotmail.com>
2022-07-20 20:32:58 -0300

    cobertura de test > 90% da pagina de progresso

--- src/tests/07.recipeInProgress.spec.js
+++ src/tests/07.recipeInProgress.spec.js
@@ -50,26 +50,113 @@ describe('testando a tela de receitas em proguesso', () => {
     expect(listOfIngredients).toHaveLength(4);
   });
 
-  test('todos os ingredientes de uma receita de comida possuem um checkbox', async () => {
+  test('verifica se ao clicar em um igredientede alguam comida, ele é marcado', async () => {
     global.fetch = jest.fn().mockImplementation(mockFetch);
     const { history } = renderWithRouter(<App />);
 
     history.push('foods/52977/in-progress');
     await waitFor(() => expect(global.fetch).toHaveBeenCalled());
-    const ingredients = mealFormat(CORBA.meals)[0].ingredients;
-    
-    const listaDeIngredientes = [];
-    const allCheckBox = Array.from(screen.getAllByRole('checkbox'));
-    ingredients.forEach(cada => {
-      listaDeIngredientes.push(screen.getByText(`${cada.measure} ${cada.ingredient}`));
-    });
 
-    console.log(listaDeIngredientes);
+    const allCheckBox = Array.from(screen.getAllByRole('checkbox'));
     expect(allCheckBox.length).toBe(13);
-    console.log(allSpan[0]);
+
     allCheckBox.forEach(cada => {
       userEvent.click(cada);
       expect(cada).toBeChecked();
     });
+
+    const allSpanMarked = Array.from(
+      screen.getAllByTestId('ingredient-in-list')
+    );
+    allSpanMarked.forEach(span => {
+      expect(span).toHaveClass('checkedIngredient');
+    });
   });
+
+  test('verifica se a lista de igredientes da comida marcada é salva', async () => {
+    global.fetch = jest.fn().mockImplementation(mockFetch);
+    const { history } = renderWithRouter(<App />);
+    history.push('foods/52977/in-progress');
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+
+    const allCheckBox = Array.from(screen.getAllByRole('checkbox'));
+    allCheckBox.forEach(cada => {
+      expect(cada).toBeChecked();
+
+      const allSpanMarked = Array.from(
+        screen.getAllByTestId('ingredient-in-list')
+      );
+      allSpanMarked.forEach(span => {
+        expect(span).toHaveClass('checkedIngredient');
+      });
+    });
+  });
+
+  test('verifica se clicar novamente no igrediente de comida ele é desmarcado', async () => {
+    global.fetch = jest.fn().mockImplementation(mockFetch);
+    const { history } = renderWithRouter(<App />);
+    history.push('foods/52977/in-progress');
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+
+    const allCheckBox = Array.from(screen.getAllByRole('checkbox'));
+    allCheckBox.forEach(cada => {
+      userEvent.click(cada);
+      expect(cada).not.toBeChecked();
+
+    
+    });
+  });
+
+    test('verifica se ao clicar em um igredientede alguam bebida, ele é marcado', async ()=>{
+      global.fetch = jest.fn().mockImplementation(mockFetch);
+      const { history } = renderWithRouter(<App />);
+
+      history.push('drinks/17222/in-progress');
+      await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+      const allCheckBox = Array.from(screen.getAllByRole('checkbox'));
+      expect(allCheckBox.length).toBe(4);
+
+      allCheckBox.forEach(cada => {
+          userEvent.click(cada);
+          expect(cada).toBeChecked();
+        });
+        const allSpanMarked = Array.from(screen.getAllByTestId('ingredient-in-list'));
+      allSpanMarked.forEach((span)=>{
+          expect(span).toHaveClass('checkedIngredient')
+      });
+
+    });
+
+    test('verifica se a lista de igredientes da bebida marcada é salva', async () => {
+        global.fetch = jest.fn().mockImplementation(mockFetch);
+        const { history } = renderWithRouter(<App />);
+        history.push('drinks/17222/in-progress');
+        await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+    
+        const allCheckBox = Array.from(screen.getAllByRole('checkbox'));
+        allCheckBox.forEach(cada => {
+          expect(cada).toBeChecked();
+    
+          const allSpanMarked = Array.from(
+            screen.getAllByTestId('ingredient-in-list')
+          );
+          allSpanMarked.forEach(span => {
+            expect(span).toHaveClass('checkedIngredient');
+          });
+        });
+      });
+      test('verifica se clicar novamente no igrediente de bebida ele é desmarcado', async () => {
+        global.fetch = jest.fn().mockImplementation(mockFetch);
+        const { history } = renderWithRouter(<App />);
+        history.push('drinks/17222/in-progress');
+        await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+    
+        const allCheckBox = Array.from(screen.getAllByRole('checkbox'));
+        allCheckBox.forEach(cada => {
+          userEvent.click(cada);
+          expect(cada).not.toBeChecked();
+    
+        
+        });
+      });
 });
